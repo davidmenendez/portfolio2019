7ffb68e1b08ea10187f342c4e65212a6be262f52
David Menendez <damenend@gmail.com>
2019-08-12 00:24:04 -0500

    meta update and viewport

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,21 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.render('index', { title: 'Front End Web Developer' });
+  res.render('index', {
+    title: 'Front End Web Developer',
+    description: 'David Menendez front end web developer',
+  });
 });
 
 router.get('/about', (req, res) => {
-  res.render('about', { title: 'About' });
+  res.render('about', {
+    title: 'About',
+    description: 'About David Menendez',
+  });
 });
 
-router.get('/portfolio', (req, res) => {
-  res.render('portfolio', { title: 'portfolio' });
-});
+// router.get('/portfolio', (req, res) => {
+//   res.render('portfolio', { title: 'portfolio' });
+// });
 
 module.exports = router;
