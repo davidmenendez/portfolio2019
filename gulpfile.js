78eb2581d9df75d21dd3e3831150c44964ddb8ae
David Menendez <dmenend@us.ibm.com>
2019-08-14 15:49:57 -0500

    gulp cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -3,17 +3,17 @@ const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
 
 function style() {
-    return gulp.src('./src/scss/**/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('./public/stylesheets'))
-        .pipe(browserSync.stream());
+  return gulp.src('./src/scss/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./public/stylesheets'))
+    .pipe(browserSync.stream());
 }
 
 function watch() {
-    browserSync.init({
-        proxy: 'http://localhost:3000',
-    });
-    gulp.watch('./src/scss/**/*.scss', style);
+  browserSync.init({
+    proxy: 'http://localhost:3000',
+  });
+  gulp.watch('./src/scss/**/*.scss', style);
 }
 
 exports.style = style;
