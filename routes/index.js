2f9bee86d1f034d35be07ef00f1e1227466983a6
David Menendez <dmenend@us.ibm.com>
2019-08-21 00:31:38 -0500

    404 page

--- routes/index.js
+++ routes/index.js
@@ -3,21 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.render('index', {
-    title: 'Front End Web Developer',
-    description: 'David Menendez front end web developer',
-  });
+  res.render('index');
 });
-
-// router.get('/about', (req, res) => {
-//   res.render('about', {
-//     title: 'About',
-//     description: 'About David Menendez',
-//   });
-// });
-
-// router.get('/portfolio', (req, res) => {
-//   res.render('portfolio', { title: 'portfolio' });
-// });
 
 module.exports = router;
